91c36aaf7f2c940bdafe0393f93b2b236e2a2621
정현태 <jht3021@gmail.com>
2026-01-29 00:50:05 +0900

    feat : header 컴포넌트 구현

--- src/pages/components.tsx
+++ src/pages/components.tsx
@@ -1,9 +1,13 @@
 import Footer from "@/widgets/footer";
+import HeaderNotLogin from "@/widgets/header/header_not_login";
+import HeaderLogin from "@/widgets/header/header_login";
 
 const Components = () => {
     return (
         <>
             <Footer />
+            <HeaderNotLogin />
+            <HeaderLogin status="home" />
         </>
     );
 };
---
+++ src/widgets/header/header_not_login.tsx
@@ -0,0 +1,76 @@
+import styled from '@emotion/styled';
+import logo from '@/shared/assets/logo.png'
+
+export const Frame = styled.header`
+  display: flex;
+
+  width: 100%;
+  height: 8rem;
+
+  align-items: center;
+  justify-content: center;
+
+
+
+`;
+
+export const Content = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 96rem;
+  height:4rem;
+
+
+
+  align-items: center;
+`;
+
+export const Logo = styled.div`
+  width: 5.75rem;
+  height: 2rem;
+  aspect-ratio: 1 / 1;
+  img {
+    width: 100%;
+    height: 100%;
+        object-fit: contain;
+    }
+  cursor: pointer;
+`;
+
+export const LoginButton = styled.button`
+    width:7.7rem;
+    height:3.5rem;
+    font-family: Pretendard;
+    font-size: 1.3rem;
+
+    border:none;
+    border-radius:8px;
+    text-align: center;
+    color:${({ theme }) => theme.colors.default.white};
+    background-color:${({ theme }) => theme.colors.primary[200]};
+
+    &:hover {
+        background-color: ${({ theme }) => theme.colors.primary[250]};
+    }
+
+    &:active {
+        background-color: ${({ theme }) => theme.colors.primary[300]};
+        transform: scale(0.96);
+    }
+`;
+
+
+const Header = () => {
+    return (
+        <Frame>
+            <Content>
+                <Logo>
+                    <img src={logo} alt="logo" />
+                </Logo>
+                <LoginButton>로그인</LoginButton>
+            </Content>
+        </Frame>
+    );
+};
+
+export default Header;
