a7aef9fb526af8e483b860648e3ce3b32dae3ac4
정현태 <jht3021@gmail.com>
2026-01-30 18:50:36 +0900

    feat : 입력창 컴포넌트 구현

--- src/pages/components.tsx
+++ src/pages/components.tsx
@@ -2,6 +2,7 @@ import Footer from "@/widgets/footer";
 import HeaderNotLogin from "@/widgets/header/header_not_login";
 import HeaderLogin from "@/widgets/header/header_login";
 import Notice from "@/widgets/notice";
+import InputDefault from "@/widgets/input_default";
 
 const Components = () => {
     return (
@@ -10,6 +11,7 @@ const Components = () => {
             <HeaderNotLogin />
             <HeaderLogin status="home" />
             <Notice />
+            <InputDefault placeholder="placeholder" />
         </>
     );
 };
