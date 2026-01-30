f90f64a0f9746a8249d95fb42e04bc3aa2d38903
정현태 <jht3021@gmail.com>
2026-01-30 16:36:00 +0900

    feat : 검색바 컴포넌트 완성

--- src/pages/components.tsx
+++ src/pages/components.tsx
@@ -1,6 +1,7 @@
 import Footer from "@/widgets/footer";
 import HeaderNotLogin from "@/widgets/header/header_not_login";
 import HeaderLogin from "@/widgets/header/header_login";
+import SearchBar from "@/widgets/search_bar";
 
 const Components = () => {
     return (
@@ -8,6 +9,7 @@ const Components = () => {
             <Footer />
             <HeaderNotLogin />
             <HeaderLogin status="home" />
+            <SearchBar width="24.5" placeholder="직무, 회사 키워드로 검색하세요" />
         </>
     );
 };
