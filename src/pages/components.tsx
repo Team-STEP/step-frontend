a366ee1e80e11c233b52b408a322007baf1fc482
정현태 <jht3021@gmail.com>
2026-02-01 18:13:58 +0900

    feat : 홈 화면의 공고 카드 구현

--- src/pages/components.tsx
+++ src/pages/components.tsx
@@ -5,6 +5,7 @@ import SearchBar from "@/widgets/search_bar";
 import Notice from "@/widgets/notice";
 import InputDefault from "@/widgets/input_default";
 import MyProcess from "@/widgets/myprocess";
+import AnnouncementMain from "@/widgets/announcement_main";
 
 
 const Components = () => {
@@ -16,6 +17,7 @@ const Components = () => {
             <SearchBar width="24.5" placeholder="직무, 회사 키워드로 검색하세요" />
             <Notice />
             <MyProcess status="first" />
+            <AnnouncementMain />
             <Footer />
         </>
     );
