a518f61c429c74ef1f045b6bc281237250b8bba4
정현태 <jht3021@gmail.com>
2026-02-14 18:54:54 +0900

    chore : 라우팅 기본 설정

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,14 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { ThemeProvider } from "@emotion/react";
+import { RouterProvider } from "react-router-dom";
 
-import Components from "../pages/components";
+import { router } from "./router.tsx";
 import { theme } from "@/shared/styles/theme.ts";
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
         <ThemeProvider theme={theme}>
-            <Components />
+            <RouterProvider router={router} />
         </ThemeProvider>
     </StrictMode>
 );
